6ea22b70cf472f45bb101c088c0f83c90c7e4a55
Damir Arh <damir.arh@gmail.com>
2022-10-29 13:05:57 +0200

    Add project files.

---
+++ CsForeachLoopVariable/Program.cs
@@ -0,0 +1,22 @@
+﻿var attributes = typeof(MyClass).GetCustomAttributes(typeof(FirstAttribute), false);
+
+foreach (SecondAttribute attribute in attributes)
+{
+    //...
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class FirstAttribute : Attribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class)]
+public class SecondAttribute : Attribute
+{
+}
+
+[First]
+[Second]
+public class MyClass
+{
+}
